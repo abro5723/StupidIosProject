e7c94d60c7603f84ae65eb3e0e1f11617bb598a2
Brown, Ashton <abro5723@gmail.com>
2015-12-17 08:37:25 -0700

    Created a stupid project with many UI components

--- StupidIosProject/ViewController.swift
+++ StupidIosProject/ViewController.swift
@@ -8,8 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
+    
+    @IBAction func Switch(sender: AnyObject)
+    {
+        randomColorChanges()
+    }
+    @IBAction func Button(sender: UIButton)
+    {
+        randomColorChanges()
+    }
+        
+    func randomColorChanges()
+    {
+        let newRed = CGFloat(Double(arc4random_uniform(256))/255.000)
+        
+        let newBlue = CGFloat(Double(arc4random_uniform(256))/255.000)
+
+        let newGreen = CGFloat(Double(arc4random_uniform(256))/255.000)
+        
+        view.backgroundColor = UIColor(red: newRed, green: newGreen, blue: newBlue, alpha: 1.0)
+
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
